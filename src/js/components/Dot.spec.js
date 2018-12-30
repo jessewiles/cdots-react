6e37e0fef2291f49f666a52909fa75e6f59409b3
Jesse Wiles <jesse.wiles@gmail.com>
2018-12-30 01:17:06 -0500

    Something circle can be happy with

--- src/js/components/Dot.spec.js
+++ src/js/components/Dot.spec.js
@@ -7,7 +7,7 @@ import Dot from './Dot.js';
 let adot = {
     id: "abcd",
     content: "this is content",
-    start: new Date("2015-01-01")
+    start: new Date("2015-01-02")
 };
 
 describe('<Dot />', function () {
@@ -17,6 +17,6 @@ describe('<Dot />', function () {
     });
     it('should have start', () => {
         const wrapper = shallow(<Dot dot={adot} />);
-        expect(wrapper.find('div').first().html()).to.include('<input type="text" class="form-control" value="2014-12-31"/>');
+        expect(wrapper.find('div').first().html()).to.include('<input type="text" class="form-control" value="2015-01-');
     });
 });
